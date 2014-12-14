30bdbd5e6c41b9401d55e132952b841c4fde5f87
anglee <ang.lee@gmail.com>
2014-12-14 14:15:19 -0500

    Add node upon button click

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 (function() {
   "use strict";
 
-
-  //Width and height
   var w = 300;
   var h = 300;
 
-  //Original data
+  var colors = d3.scale.category10();
+
   var dataset = {
     nodes: [
       { name: "Adam" },
@@ -43,47 +42,56 @@
       .links(dataset.edges)
       .size([w, h])
       .linkDistance([50])
-      .charge([-100])
-      .start();
+      .charge([-100]);
 
-  var colors = d3.scale.category10();
+  force.on("tick", function() {
+    edges.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+    nodes.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
 
-  //Create SVG element
   var svg = d3.select("body")
       .append("svg")
       .attr("width", w)
       .attr("height", h);
 
-  //Create edges as lines
-  var edges = svg.selectAll("line")
-      .data(dataset.edges)
-      .enter()
-      .append("line")
-      .style("stroke", "#ccc")
-      .style("stroke-width", 1);
+  var edges = svg.selectAll("line");
+  var nodes = svg.selectAll("circle");
 
-  //Create nodes as circles
-  var nodes = svg.selectAll("circle")
-      .data(dataset.nodes)
-      .enter()
-      .append("circle")
-      .attr("r", 10)
-      .style("fill", function(d, i) {
-        return colors(i);
-      })
-      .call(force.drag);
+  var start = function() {
+    edges = edges.data(dataset.edges);
+    edges.enter()
+        .append("line")
+        .style("stroke", "#ccc")
+        .style("stroke-width", 1);
+    nodes = nodes.data(dataset.nodes);
+    nodes.enter()
+        .append("circle")
+        .attr("r", 10)
+        .style("fill", function(d, i) {
+          return colors(i);
+        })
+        .call(force.drag);
 
-  //Every time the simulation "ticks", this will be called
-  force.on("tick", function() {
+    force.start();
+  };
 
-    edges.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+  setTimeout(function() {
+    start();
+  }, 0);
 
-    nodes.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-
+  d3.select("#btn").on("click", function() {
+    dataset.nodes.push({
+      name: "Ang"
+    });
+    dataset.edges.push({
+      source: dataset.nodes.length - 1,
+      target: 0
+    });
+    start();
   });
 
 })();
